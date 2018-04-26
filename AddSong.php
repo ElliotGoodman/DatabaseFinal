94d4287054b30b72e44ae2664a4d902cfa9108b3
ElliotGoodman <38385191+ElliotGoodman@users.noreply.github.com>
2018-04-26 14:09:43 -0500

    Add files via upload
    youtubeLink field allows colon, ampersand, equals sign, forward slash, period, underscore, and alphanumerics
    artist field allows space, colon, ampersand, forward slash, period, and alphanumerics allowed
    name only allows alphanumerics

--- AddSong.php
+++ AddSong.php
@@ -5,23 +5,32 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"><!-- Optional theme -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <title>Add A Song! </title>
+<style>
+.center
+  {
+  text-align:center;
+  }
+</style>
 </head>
 <body>
+  <div class="center">
 <h1>Add A Song to Our Database! </h1>
 
 <form action="" method="POST">
 
-<p><medium> What's its name?</medium></p>
-<small>Only alphanumeric characters and spaces are allowed, up to 50</small>
-<br>
+<br><br>
+<p><medium><font size="+1"> What's its name?</font></medium></p>
 
-<input type="text" name"songName"
+<input type="text" name="songName"
 placeholder="Song name here" required
 pattern="[a-zA-Z0-9^\s]{1,50}"/> <!--between 1 and 50 alphanumeric characters-->
+<br>
+<small>Only alphanumeric characters and spaces are allowed, up to 50</small>
 
 <br>
-Pick its genre
 <br>
+<p>Pick its genre</p>
+
   <select name="Genres" id="Genres" required>
     <option value="" disabled selected>Choose a genre</option>
     <option value="Rock">Rock</option>
@@ -29,12 +38,11 @@ Pick its genre
     <option value="Ukelele">Ukelele</option>
     <option value="Theatre Nerd">Theatre Nerd</option>
     <option value="Alternative">Alternative</option>
-    <option value="Pop?">Pop</option>
+    <option value="Pop">Pop</option>
   </select>
 
-<br>
-Pick its mood:
-<br>
+<br><br>
+<p>Pick its mood:</p>
   <select name="Moods" id="Moods" required>
     <option value="" disabled selected>Choose a mood</option>
     <option value="Happy">Happy</option>
@@ -49,16 +57,56 @@ Pick its mood:
     <option value="Naughty">( ͡° ͜ʖ ͡°)</option>
 </select>
 
+<br><br>
+<p>Who's the artist?</p>
+<input type="text" name="artist"
+placeholder="Artist here"
+pattern="[a-zA-Z0-9^\s\x3A\x26?!\x2F\x2E]{1,30}"/><!--\x26 stands for ampersand, \s stands for space,\x2E for period-->
 <br>
+<small>Alphanumeric characters, !, and ? only, up to 30 characters total</small>
+
+<br><br>
 <p>Add a Youtube link if possible</p>
-<br>
 <input type="text" name="youtubeLink"
 placeholder="Link here"
-pattern="[a-zA-Z0-9_-=&?/]{1,200}"/>
+pattern="[a-zA-Z0-9\x3A\x26\x3D?\x2F\x2E\x5F]{1,200}"/><!--this is allowing single and double quotes for some reason????-->
 
 <br><br>
 <input name="ADD" value="ADD" type="submit">
 </form>
 
+<?php
+if(isset($_POST['ADD']))
+   {//connect to database
+   $mysqli = new mysqli('sql209.epizy.com', 'epiz_21511524', 'xa8uCY2sZ9iF', 'epiz_21511524_MusicMoodBoard');
+            if ($mysqli->connect_errno)   //error checker
+                {
+                // echo "Failed to connect to MySQLI"
+                die("Failed to connect to MySQLI");
+                }
+            else
+    }
+if(isset($_POST['songName']))
+  {
+  echo "Song name is set!";
+  }
+else {
+     "Song name is not set.";
+     }
+
+$genre = $_POST['Genres'];
+$artist = $_POST['artist'];
+$mood = $_POST['Moods'];
+$link = $_POST['youtubeLink'];
+
+$mainQuery = "INSERT INTO mainTable (songName, genre, artist, youtubeLink) VALUES ('" . $_POST['songName'] . "','$genre','$artist','$link');";
+$mainResult = $mysqli->query($mainQuery);
+
+$moodQuery = "INSERT INTO moods (songName, mood) VALUES ('" . $_POST['songName'] . "','$mood');";
+$moodResult = $mysqli->query($moodQuery);
+
+//also this straight up just doesn't put anything into the SQL table.???
+?>
+  </div>
 </body>
 </html>
